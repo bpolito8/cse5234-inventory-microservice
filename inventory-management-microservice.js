fe549269b925943c96324aee106a45e119a73363
enyu <enyu@enyus-MacBook-Pro.local>
2021-10-24 14:53:10 -0400

    To heroku

--- inventory-management-microservice.js
+++ inventory-management-microservice.js
@@ -60,7 +60,7 @@ app.use(cors({
     origin: '*'
 }));
 
-app.get('/InventoryMicroservice/Inventory', async function(req, res) {
+app.get('https://cse5234-inventory-microservice.herokuapp.com/', async function(req, res) {
     res.jsonp(products);
 });
 
