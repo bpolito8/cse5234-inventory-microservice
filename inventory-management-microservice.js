bd9c0b7cc2dd7c7c9e0092b4b2df7b000ffa5f56
Michael Frank <frank.522@osu.edu>
2021-10-24 16:58:53 -0400

    removed hardcoded data

--- inventory-management-microservice.js
+++ inventory-management-microservice.js
@@ -1,72 +1,45 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
-var bodyParser = require('body-parser');
+const { Client } = require("pg");
+
+var bodyParser = require("body-parser");
 // create application/json parser
 var jsonParser = bodyParser.json();
 
-const products = [
-    {
-      id: 1,
-      name: 'Aoenium',
-      price: 7.99,
-      description:
-      'Aeoniums are most commonly known for their striking rosettes made up of dense, glossy, waxy leaves growing out of a single stem.',
-      quantity: 0,
-      img_src: "assets/imgs/aeonium.jpg",
-    },
-    {
-      id: 2,
-      name: 'Kalanchoe',
-      price: 6.99,
-      description:
-        'Kalanchoe is a beautiful flowering plant that is both easy and rewarding to grow indoors or outdoors.',
-      quantity: 0,
-      img_src: "assets/imgs/kalanchoe.jpg",
- 
-    },
-    {
-      id: 3,
-      name: 'Senecio',
-      price: 2.99,
-      description: 'Senecio is a genus of flowering plants that contains more than 1000 species of groundsels, ragworts, and dusty miller.',
-      quantity: 0,
-      img_src: "assets/imgs/senecio.jpg",
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-    },
-    {
-      id: 4,
-      name: 'Sempervivum',
-      price: 6.35,
-      description: 'Sempervivum is a genus of about 40 species of flowering plants that are commonly known as Houseleeks or Hen and Chicks.',
-      quantity: 0,
-      img_src: "assets/imgs/sempervivum.jpg",
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-    },
-    {
-      id: 5,
-      name: 'Crassula',
-      price: 7.49,
-      description: 'Crassula is a genus of succulent plants that includes more than 350 species native to many different parts of the world.',
-      quantity: 0,
-      img_src: "assets/imgs/crassula.jpg",
+client.connect();
 
-    },
-  ];
-
-const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
-
-app.get('/InventoryMicroservice/Inventory', async function(req, res) {
-    res.jsonp(products);
+app.get("/InventoryMicroservice/Inventory", async function (req, res) {
+  client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    }
+  );
+  res.jsonp(products);
 });
 
 var server = app.listen(port, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log(`Example app listening at ${host}:${port}`)
-
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`Example app listening at ${host}:${port}`);
 });
